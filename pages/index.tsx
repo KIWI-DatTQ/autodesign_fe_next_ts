d8e3a6a92fb679efd2a8d10a0525149c68f0da88
dattrinhquoc <dattrinhquoc112@gmail.com>
2023-07-07 14:55:27 +0700

    Test keio

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import ButtonCustom from "@components/Button";
 
 const Home = () => {
+  const IOS_URL =
+    "https://apps.apple.com/jp/app/%E4%BA%AC%E7%8E%8B%E3%82%A2%E3%83%97%E3%83%AA/id1078553611";
   const openApp = () => {
     try {
       const url = "keio-train-app://member_card";
       window.location.href = url;
-    } catch (error) {
-      window.location.href =
-        "https://apps.apple.com/jp/app/%E4%BA%AC%E7%8E%8B%E3%82%A2%E3%83%97%E3%83%AA/id1078553611";
-    }
+      setTimeout(() => {
+        window.location.href = IOS_URL;
+      }, 250);
+    } catch (error) {}
   };
   useEffect(() => {
     openApp();
