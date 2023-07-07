c01893deef9f7c38649eb24b1ef291766ba261d8
dattrinhquoc <dattrinhquoc112@gmail.com>
2023-07-07 11:30:56 +0700

    Test Keio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ImportComponent from "@pages/ImportComponent";
+import ButtonCustom from "@components/Button";
 
 const Home = () => {
+  const openApp = () => {
+    try {
+      const url = "keio-train-app://member_card";
+      window.location.href = url;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    openApp();
+  }, []);
   return (
     <>
-      <ImportComponent />
+      <ButtonCustom onClick={openApp}>Open Link</ButtonCustom>
     </>
   );
 };
