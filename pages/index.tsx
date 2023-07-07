ab74e8a5dbf26f79c0e73030678260f89b6ebc99
dattrinhquoc <dattrinhquoc112@gmail.com>
2023-07-07 15:14:10 +0700

    Test Keio

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,22 @@ const Home = () => {
     "https://apps.apple.com/jp/app/%E4%BA%AC%E7%8E%8B%E3%82%A2%E3%83%97%E3%83%AA/id1078553611";
   const openApp = () => {
     try {
-      const url = "keio-train-app://member_card";
-      window.location.href = url;
-      setTimeout(() => {
-        window.location.href = IOS_URL;
-      }, 250);
+      const pageLink = window.location.href;
+      const query = new URLSearchParams(window.location.search);
+      const isOpened = query.get("opened");
+      if (!isOpened) {
+        const url = "keio-train-app://member_card";
+        window.location.href = url;
+        query.set("opened", "true");
+        setTimeout(() => {
+          window.location.href = IOS_URL;
+          setTimeout(() => {
+            window.location.search = query.toString();
+          }, 250);
+        }, 250);
+        console.log(pageLink);
+        console.log(query.get("opened"));
+      }
     } catch (error) {}
   };
   useEffect(() => {
